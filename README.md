f180e24fae9ac7ab8b195b9c0d932a31c29f32d9
Cristian049 <suiucristian04@yahoo.com>
2024-11-28 23:13:04 +0200

    Commit from laptop 28 nov

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # WeatherSite
 
 Weather site from pc
-<<<<<<< HEAD
-Weather site from Laptop
-=======
 Started 22 Nov 2024
 Modified on 26 Nov
->>>>>>> 53798258dad5fbc7b08523931031d7d54e0d7c39
+Modified on 28 Nov laptop
--- app.js
+++ app.js
@@ -5,18 +5,66 @@
 //https://api.weatherbit.io/v2.0/current?city=Craiova&country=RO&key=0b5eac87736d4cab95709976b8c2e278 API CRAIOVA
 //  /forecast/daily --> daily forecast
 // /current --> current forecast
+// /forecast/hourly --> hourly forecast
 // accuweather.com
-const weatherApiKey = "0b5eac87736d4cab95709976b8c2e278";
-
-const weatherUrl = `https://api.weatherbit.io/v2.0/current?city=${city}&key=${weatherApiKey}`;
-
+const inputSearch = document.querySelector("input");
 const form = document.querySelector("form");
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  form.reset();
-});
+const weatherContainer = document.querySelector(".weather");
 
 // Fetch weather data for Craiova when the page loads
 // window.onload = function () {
 //   fetchWeatherData(); // Fetch default weather for Craiova
 // };
+
+class WeatherDataDaily {
+  constructor(apiResponse) {
+    const dataRes = apiResponse.data[0];
+    this.city = dataRes.city_name;
+    this.temperature = dataRes.temp;
+    this.condition = dataRes.weather.description;
+    this.icon = dataRes.weather.icon;
+  }
+  getWeatherIcon() {
+    return `https://www.weatherbit.io/static/img/icons/${this.icon}.png`;
+  }
+}
+
+class WeatherCardDaily {
+  constructor(weatherData) {
+    this.weatherData = weatherData;
+  }
+  render() {
+    const card = document.createElement("div");
+    card.className = "weather-card";
+
+    card.innerHTML = `
+        <h2>${this.weatherData.city}</h2>
+        <p>${this.weatherData.temperature}°C</p>
+        <p>${this.weatherData.condition}</p>
+        <img src="${this.weatherData.getWeatherIcon()}" alt="${
+      this.weatherData.condition
+    }">
+    `;
+
+    return card;
+  }
+}
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const apiKey = "0b5eac87736d4cab95709976b8c2e278";
+  const baseUrl = "https://api.weatherbit.io/v2.0/current";
+  const inputVal = inputSearch.value;
+  if (inputVal) {
+    try {
+      const res = await axios.get(`${baseUrl}?city=${inputVal}&key=${apiKey}`);
+      const weatherData = new WeatherDataDaily(res.data);
+      const weatherCard = new WeatherCardDaily(weatherData);
+      weatherContainer.innerHTML = "";
+      weatherContainer.append(weatherCard.render());
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  }
+  form.reset();
+});
