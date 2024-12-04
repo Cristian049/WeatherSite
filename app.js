99deab2e703990ba756f73b2543edeec039d5a38
Cristian049 <suiucristian04@yahoo.com>
2024-12-04 11:27:56 +0200

    Changes pc

--- app.js
+++ app.js
@@ -16,17 +16,59 @@ const weatherContainer = document.querySelector(".weather");
 //   fetchWeatherData(); // Fetch default weather for Craiova
 // };
 
+function formatTime() {
+  const now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  const ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  const formattedHours = hours < 10 ? "0" + hours : hours;
+  const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+  return `${formattedHours}:${formattedMinutes} ${ampm}`;
+}
+
+function getWeatherCardClass(condition) {
+  switch (condition.toLowerCase()) {
+    case "clear sky":
+      return "clear-day";
+    case "few clouds":
+    case "scattered clouds":
+    case "broken clouds":
+    case "overcast clouds":
+      return "cloudy";
+    case "light rain":
+    case "rain":
+    case "shower rain":
+      return "rainy";
+    case "snow":
+    case "light snow":
+      return "snowy";
+    case "thunderstorm":
+      return "thunderstorm";
+    case "fog":
+    case "mist":
+      return "foggy";
+    case "wind":
+    case "windy":
+      return "windy";
+    default:
+      return "night";
+  }
+}
 class WeatherDataDaily {
   constructor(apiResponse) {
     const dataRes = apiResponse.data && apiResponse.data[0];
     this.city = dataRes.city_name;
-    this.temperature = dataRes.temp;
+    this.temperature = Math.floor(dataRes.temp);
     this.condition = dataRes.weather.description;
     this.icon = dataRes.weather.icon;
     this.country = dataRes.country_code;
     this.timezone = dataRes.timezone;
     this.humidity = dataRes.rh;
     this.windSpeed = dataRes.wind_spd;
+    this.realTemp = Math.floor(dataRes.app_temp);
+    this.pressure = dataRes.pres;
   }
   getWeatherIcon() {
     return `https://www.weatherbit.io/static/img/icons/${this.icon}.png`;
@@ -38,33 +80,47 @@ class WeatherCardDaily {
     this.weatherData = weatherData;
   }
   render() {
-    const timezone = this.weatherData.timezone || "UTC";
-    const localTime = new Date().toLocaleString("en-US", {
-      timeZone: timezone,
-    });
+    const countryCode = this.weatherData.country;
+    const currentTime = formatTime();
     const card = document.createElement("div");
-    card.className = "weather-card";
+    const weatherClass = getWeatherCardClass(this.weatherData.condition);
+    card.className = `weather-card ${weatherClass}`;
 
     card.innerHTML = `
        <div class="weather-header">
-        <h5 id="city-name">${this.weatherData.city}</h5 >
-        <h5 id="local-time">${localTime}</h5>
+        <p id="city-name">${this.weatherData.city}, ${countryCode}</p >
+        <span id="local-time">${currentTime}</span>
       </div>
-      <div class="weather-info">
-        <img id="weather-icon" src="${this.weatherData.getWeatherIcon()}" alt="${
+      <div class="weather-value">
+        <div id="weather-info">
+          <div class="weather-temps">
+            <img class="weather-icon" src="${this.weatherData.getWeatherIcon()}" alt="${
       this.weatherData.condition
     }">
-        <p id="temperature">${this.weatherData.temperature}°C</p>
-        <p id="condition">${this.weatherData.condition}</p>
-      </div>
-      <div class="additional-info">
-        <p>Humidity: <span id="humidity">${
-          this.weatherData.humidity
-        }%</span></p>
-        <p>Wind: <span id="wind-speed">${
-          this.weatherData.windSpeed
-        } km/h</span></p>
-      </div>
+          <div class="weather-temp">
+            <span class="temperature">${
+              this.weatherData.temperature
+            }<span class="temp-circle">°</span></span>
+            <span class="celsius">C</span>
+            <p class="real-feel">Real feel:${
+              this.weatherData.realTemp
+            }<span>°</span></p>
+            </div>
+          </div>
+            <p id="condition">${this.weatherData.condition}</p>
+        </div>
+        <div class="additional-info">
+           <p class="spaced-content"><span class="weather-description">Humidity</span> <span class="table-value">${
+             this.weatherData.humidity
+           }%</span></p>
+           <p class="spaced-content"><span class="weather-description">Wind</span> <span class="table-value">${
+             this.weatherData.windSpeed
+           } km/h</span></p>
+           <p class="spaced-content"><span class="weather-description">Pressure</span><span class="table-value">${
+             this.weatherData.pressure
+           } hPa</span></p>
+        </div>
+      <div>
     `;
 
     return card;
@@ -81,7 +137,7 @@ form.addEventListener("submit", async function (e) {
       const res = await axios.get(`${baseUrl}?city=${inputVal}&key=${apiKey}`);
       const weatherData = new WeatherDataDaily(res.data);
       const weatherCard = new WeatherCardDaily(weatherData);
-      weatherContainer.innerHTML = "";
+
       weatherContainer.append(weatherCard.render());
     } catch (error) {
       console.error("Error fetching weather data:", error);
