04460d0fdef3e995cd9aaa045f97b937e93cf47b
Cristian049 <suiucristian04@yahoo.com>
2025-01-09 00:33:27 +0200

    push laptop terms privacy cookie style

---
+++ privacy.js
@@ -0,0 +1,31 @@
+const terms = document.querySelector(".terms");
+const privacy = document.querySelector(".privacy");
+const cookie = document.querySelector(".cookie");
+
+terms.addEventListener("click", () => {
+  const newWindow = window.open("term.html", "_self");
+  newWindow.focus();
+  newWindow.onload = () => {
+    newWindow.document.querySelector(".terms").classList.add("btn-active");
+  };
+});
+
+privacy.addEventListener("click", () => {
+  const newWindow = window.open("privacy.html", "_self");
+  newWindow.focus();
+  newWindow.onload = () => {
+    newWindow.document.querySelector(".privacy").classList.add("btn-active");
+  };
+});
+
+cookie.addEventListener("click", () => {
+  const newWindow = window.open("cookie.html", "_self");
+  newWindow.focus();
+  newWindow.onload = () => {
+    newWindow.document.querySelector(".cookie").classList.add("btn-active");
+  };
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  privacy.classList.add("btn-active");
+});
